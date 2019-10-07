e3ed81a37132a7281206f746523a6122136cc41c
Tess Lameyer <tjlameyer@gmail.com>
2019-10-07 16:45:07 -0700

    made my data.js file

---
+++ src/data/data.js
@@ -0,0 +1,54 @@
+const truckerJacket = {
+    id:'trucker-jacket',
+    name: 'Trucker Jacket',
+    // image:
+    description: 'a casual jacket',
+    category: 'jacket',
+    price: 300.00
+};
+
+const hippiePant = {
+    id:'hippie-pant',
+    name: 'Hippie Pant',
+    // image:
+    description: 'a comfortbale unisex pant',
+    category: 'pant',
+    price: 200.00
+};
+
+const shawlCollarJacket = {
+    id:'shawl-collar-jacket',
+    name: 'Shawl Collar Jacket',
+    // image:
+    description: 'a casual jacket',
+    category: 'jacket',
+    price: 250.00
+};
+
+const hoodie = {
+    id:'hoodie',
+    name: 'Hoodie',
+    // image:
+    description: 'a comfortbale hoodie',
+    category: 'athleisure',
+    price: 80.00
+};
+
+const poncho = {
+    id:'poncho',
+    name: 'poncho',
+    // image:
+    description: 'a traditional Mexican garmet',
+    category: 'poncho',
+    price: 200.00
+};
+
+const clothing = [
+    truckerJacket, 
+    hippiePant,
+    shawlCollarJacket,
+    hoodie, 
+    poncho
+];
+
+export default clothing;
