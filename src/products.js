d0e1b5dff050626c06f11e5fa27a30051becfd46
Tess Lameyer <tjlameyer@gmail.com>
2019-10-07 22:42:30 -0700

    got products.js working, clothing objects correctly rendered

---
+++ src/products.js
@@ -0,0 +1,10 @@
+import clothing from './data/data.js';
+import renderClothes from './render-clothes.js';
+
+const list = document.getElementById('clothing');
+
+for (let i = 0; i < clothing.length; i++) {
+    const clothingResult = clothing[i];
+    const dom = renderClothes(clothingResult);
+    list.appendChild(dom);
+}
