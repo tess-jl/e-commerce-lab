fada3d05542259f22b256717e9899095343c765d
Tess Lameyer <tjlameyer@gmail.com>
2019-10-08 15:47:11 -0700

    got table working

--- common/utils.js
+++ common/utils.js
@@ -1,8 +1,16 @@
 export const makePrettyCurrency = (number) => {
-    const prettyCurrency = number.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-    return prettyCurrency;
+    const makePrettyCurrency = number.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    return makePrettyCurrency; 
+
 };
 
+const totalClothingPrice = (clothting, quantity) => { // should have a TDD
+    clothing.price * quantity; 
+};
+
+export const totalCartPrice = (clothing, order) => {
+    
+};
 
 
 
