88c6b9186c4fcb4b3a44eb1dbe30ece9e61e3c43
Tess Lameyer <tjlameyer@gmail.com>
2019-10-09 17:33:32 -0700

    setting up event hander for render clothing

--- products/render-clothes.js
+++ products/render-clothes.js
@@ -1,3 +1,43 @@
+const ORDER_KEY = 'order'; //magic string
+import { findById } from '../common/utils.js';
+
+
+
+const initializeEmptyOrder (() => {
+    const serializedOrder = JSON.stringify(emptyOrder); // making an object a string to store locally 
+    localStorage.setItem(ORDER_KEY, serializedOrder);
+});
+
+const getOrder = () => JSON.parse(localStorage.getItem(ORDER_KEY));
+
+const incrementOrderById = (id, order) => {
+
+    let itemAlreadyInOrder = false;
+
+    order.forEach(newOrder => {
+        //if the ids match
+        if (order.id === newOrder.id) {
+            //increment the quantity of the order item 
+            itemAlreadyInOrder = true; 
+            newOrder.quantity++; 
+        }
+    });
+
+    if (itemAlreadyInOrder) {
+        return; // break function
+    } else {
+        const newOrderItem = { 
+            id: id,
+            quantity: 1 //,
+        };
+        order.push(newOrderItem);
+    }
+};
+
+
+
+
+
 function renderClothes(clothing) {
     const li = document.createElement('li');
     li.className = clothing.category;
@@ -22,11 +62,34 @@ function renderClothes(clothing) {
     const button = document.createElement('button');
     button.textContent = 'add to cart';
     button.value = clothing.id;
-    p.appendChild(button);
+   
+    button.addEventListener('click', () => {
 
+        // retrieve existing shopping cart from the localStorage
+        let orderLocalStorage = getOrder();
+
+        // if no order data, then set the cart data of localStorage to an empty array (initialize) that has been turned into a string
+        if (!orderLocalStorage) {
+            initializeEmptyOrder();
+            currentOrder = getOrder();
+        } 
+
+        // if there is already order data, then need to increment through the array 
+        let clothingToIncrement = findById(currentOrder, clothing.id);
+
+        
+
+
+
+        
+
+        //check if the 
+
+    });
+    p.appendChild(button);
     li.appendChild(p);
 
     return li;
-}
+};
 
 export default renderClothes;
