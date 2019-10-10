0ddfe154c72920bdd4a29cc10c2163e241020b0f
Tess Lameyer <tjlameyer@gmail.com>
2019-10-09 21:27:17 -0700

    about to adjust shopping-cart.js for dynamically-rendered order data

--- products/render-clothes.js
+++ products/render-clothes.js
@@ -1,40 +1,69 @@
-const ORDER_KEY = 'order'; //magic string
 import { findById } from '../common/utils.js';
 
+const ORDER_KEY = 'order'; //magic string
+const emptyOrder = [];
 
 
-const initializeEmptyOrder (() => {
+
+const initializeEmptyOrder = () => {
     const serializedOrder = JSON.stringify(emptyOrder); // making an object a string to store locally 
     localStorage.setItem(ORDER_KEY, serializedOrder);
-});
+};
 
 const getOrder = () => JSON.parse(localStorage.getItem(ORDER_KEY));
 
-const incrementOrderById = (id, order) => {
 
-    let itemAlreadyInOrder = false;
 
-    order.forEach(newOrder => {
-        //if the ids match
-        if (order.id === newOrder.id) {
-            //increment the quantity of the order item 
-            itemAlreadyInOrder = true; 
-            newOrder.quantity++; 
-        }
-    });
+// const incrementOrderById = (id, order) => {
 
-    if (itemAlreadyInOrder) {
-        return; // break function
-    } else {
-        const newOrderItem = { 
-            id: id,
-            quantity: 1 //,
+//     let itemAlreadyInOrder = false;
+
+//     order.forEach(newOrder => {
+//         //if the ids match
+//         if (order.id === newOrder.id) {
+//             //increment the quantity of the order item 
+//             itemAlreadyInOrder = true; 
+//             newOrder.quantity++; 
+//         }
+//     });
+
+//     if (itemAlreadyInOrder) {
+//         return; // break function
+//     } else {
+//         const newOrderItem = { 
+//             id: id,
+//             quantity: 1 //,
+//         };
+//         order.push(newOrderItem);
+//     }
+// };
+
+
+
+//make a new function that uses the findById and if it doesn't find something it will create new order item and push into the cart
+const pushCorrectOrderItem = (currentOrder, clothingId) => {
+
+    //findById returns clothing item 
+    const existingClothingItem = findById(currentOrder, clothingId);
+    if (existingClothingItem === null) {
+        const newOrderItem = {
+            id: clothingId,
+            quantity: 1
         };
-        order.push(newOrderItem);
+        currentOrder.push(newOrderItem);
+    } else {
+        const updatedClothingItem = {
+            id: clothingId,
+            quantity: 1
+        };
+        currentOrder.push(updatedClothingItem);
     }
 };
 
-
+const setOrder = (currentOrderInLocalStorage) => {
+    const serializedNewCart = JSON.stringify(currentOrderInLocalStorage);
+    localStorage.setItem(ORDER_KEY, serializedNewCart);
+};
 
 
 
@@ -62,34 +91,28 @@ function renderClothes(clothing) {
     const button = document.createElement('button');
     button.textContent = 'add to cart';
     button.value = clothing.id;
-   
+
     button.addEventListener('click', () => {
 
         // retrieve existing shopping cart from the localStorage
-        let orderLocalStorage = getOrder();
+        let orderLocalStorage = getOrder(); //returns array of order so far
 
-        // if no order data, then set the cart data of localStorage to an empty array (initialize) that has been turned into a string
+        // if no order data in local storage, then set the cart data of localStorage to an empty array (initialize) that has been turned into a string
         if (!orderLocalStorage) {
-            initializeEmptyOrder();
-            currentOrder = getOrder();
-        } 
-
-        // if there is already order data, then need to increment through the array 
-        let clothingToIncrement = findById(currentOrder, clothing.id);
-
+            initializeEmptyOrder(); 
+            orderLocalStorage = getOrder(); //turns data into an array
+            pushCorrectOrderItem(orderLocalStorage, orderLocalStorage.id);
+        } else {
+            pushCorrectOrderItem(orderLocalStorage, orderLocalStorage.id);
+        }
         
-
-
-
-        
-
-        //check if the 
+        setOrder(orderLocalStorage);
 
     });
     p.appendChild(button);
     li.appendChild(p);
 
     return li;
-};
+}
 
 export default renderClothes;
